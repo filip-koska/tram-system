f043d279ab8fa8bbd1da8f33e05945506da8909c
filip-koska <filip2k4@gmail.com>
2025-02-18 17:03:24 +0100

    Update Wektor.java

--- src/pojemnik/Wektor.java
+++ src/pojemnik/Wektor.java
@@ -1,12 +1,16 @@
 package pojemnik;
 
-// Interfejs opisujący funkcjonalność tablicy dynamicznej
-// Ubóstwo interfejsu wynika z niepewności w stosowanu typów uogólnionych
+// Interface for a dynamic array
+// The modesty of the interface is the result of not having been yet introduced
+// to generic programming in Java
 public interface Wektor {
 
+    // isEmpty
     boolean czyPusty();
 
+    // size
     int rozmiar();
 
+    // capacity
     int pojemność();
 }
