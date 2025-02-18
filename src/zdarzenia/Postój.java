b0c8381604a5821fb9f679215dbbe6e7184147d5
filip-koska <filip2k4@gmail.com>
2025-02-18 17:18:34 +0100

    Update Postój.java

--- src/zdarzenia/Postój.java
+++ src/zdarzenia/Postój.java
@@ -4,10 +4,18 @@ import czas.Moment;
 import pojazdy.Pojazd;
 import pojemnik.KolejkaZdarzeń;
 
+// Vehicle stop event
 public class Postój extends Zdarzenie {
 
+    // data
+
+    // vehicle
     private Pojazd pojazd;
+    // stop index
     private int indeksPrzystanku;
+
+    // technicalities
+    
     public Postój(Moment moment, int indeksPrzystanku, Pojazd pojazd) {
         super(moment);
         this.pojazd = pojazd;
