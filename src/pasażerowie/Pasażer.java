cba57d888ebee04bbb361b8118db0d6e95489b89
filip-koska <filip2k4@gmail.com>
2025-02-18 15:36:08 +0100

    Changed comments' language to English

--- src/pasażerowie/Pasażer.java
+++ src/pasażerowie/Pasażer.java
@@ -9,17 +9,22 @@ import zdarzenia.DojścieRano;
 
 public class Pasażer {
 
-    // dane
+    // data
 
+    // unique ID number
     private int numer;
+    // home stop
     private Przystanek przystanekDomowy;
+    // current destination stop
     private Przystanek cel;
-    private int czasOczekiwania; //
+    // total daily waiting time
+    private int czasOczekiwania;
+    // total daily number of rides
     private int liczbaPrzejazdów;
-
+    // timestamp of the previous event
     private Moment poprzedniMoment;
 
-    // techniczne
+    // technicalities
 
     public Pasażer(int numer, DaneSymulacji daneSymulacji) {
         this.numer = numer;
@@ -28,7 +33,7 @@ public class Pasażer {
         this.czasOczekiwania = this.liczbaPrzejazdów = 0;
     }
 
-    // operacje
+    // operations
 
     @Override
     public String toString() {
@@ -55,32 +60,32 @@ public class Pasażer {
         this.cel = przystanek;
     }
 
-    // Odczytuje czas oczekiwania pasażera, a następnie resetuje go celem przygotowania pasażera do następnego dnia
+    // Reads the passenger's waiting time and resets it to prepare the passenger for the next day
     public int odczytajCzasOczekiwania() {
         int wynik = this.czasOczekiwania;
         this.czasOczekiwania = 0;
         return wynik;
     }
 
-    // Odczytuje liczbę przejazdów pasażera, a następnie resetuje ją celem przygotowania pasażera do następnego dnia
+    // Reads the passenger's total number of rides and resets it to prepare the passenger for the next day
     public int odczytajLiczbęPrzejazdów() {
         int wynik = this.liczbaPrzejazdów;
         this.liczbaPrzejazdów = 0;
         return wynik;
     }
 
-    // Porównanie pasażerów identyfikowanych przez numer
+    // Compares two passengers for equality based on their ID number
     public boolean równy(Pasażer p) {
         return this.numer == p.numer;
     }
 
-    // Obsługa przyjścia rano na przystanek domowy
+    // Handles the daily initial appearance of the passenger at their home stop
     public void przyjdźRano(KolejkaZdarzeń q, Moment moment) {
         this.poprzedniMoment = moment;
         q.wstaw(new DojścieRano(moment, this));
     }
 
-    // Wsiadanie pasażera do pojazdu z przystanku, a tym samym marker zwiększający dzienną liczbę przejazdów
+    // Registers that the passenger got on a vehicle and updates passenger statistics
     public void wsiądźDoPojazdu(Pojazd pojazd, int indeksPrzystanku, Moment moment) {
         pojazd.dodajPasażeraPojazd(this);
         this.czasOczekiwania += moment.czas() - this.poprzedniMoment.czas();
@@ -92,7 +97,7 @@ public class Pasażer {
                             " z zamiarem dojechania na przystanek " + this.cel);
     }
 
-    // Wysiadanie pasażera z pojazdu na przystanek
+    // Registers that the passenger got off a vehicle and updates passenger statistics
     public void wejdźNaPrzystanek(Pojazd pojazd, Przystanek przystanek, Moment moment) {
         przystanek.dodajPasażeraPrzystanek(this);
         this.poprzedniMoment = moment;
